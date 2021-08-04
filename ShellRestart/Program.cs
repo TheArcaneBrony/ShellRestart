8b683c0cfe6a4e6502b05fef7d83ed32f4cd13f9
The Arcane Brony <myrainbowdash949@gmail.com>
2021-08-04 23:01:21 +0200

    first commit

---
+++ ShellRestart/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace ShellRestart
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var process in Process.GetProcessesByName("explorer"))
+            {
+                process.Kill();
+            }
+            //Process.Start("explorer");
+        }
+    }
+}
